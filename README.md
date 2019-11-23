9ca684e002414f9d131e85e7194f0e27000e9177
Justin Snyder <jsnyder@condati.com>
2019-11-22 18:38:26 -0600

    Finished tutorial

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ yarn lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+https://www.youtube.com/watch?v=Wy9q22isx3U
+video on this project
---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the TodoList app v1.0.0.  It is part of 
+      the Vue crash course on Youtube.  
+      https://www.youtube.com/watch?v=Wy9q22isx3U
+    </p>
+  </div>
+</template>
